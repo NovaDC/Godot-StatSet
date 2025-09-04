85960b01cb30366c53fd0637289c07fcdd24c6f2
MarkusHammer <164206531+NovaDC@users.noreply.github.com>
2025-09-03 20:29:11 -0500

    Add documentation display fix
    Helps ensure that Godot will display the documentation of all the scripts of this plugin in the Help viewer.

--- addons/rpg_stats/plugin.gd
+++ addons/rpg_stats/plugin.gd
@@ -10,6 +10,24 @@ const PLUGIN_ICON :Texture2D = preload("./icon.svg")
 
 var _inspector:StatSetEditorInspectorPlugin = null
 
+const ENSURE_SCRIPT_DOCS:Array[Script] = [
+	preload("./prototype/stat_prototype.gd"),
+	preload("./prototype/stat_prototype_numeric.gd"),
+	preload("./sets/inspector_plugin.gd"),
+	preload("./sets/stat_set.gd"),
+	preload("./sets/stat_set_display_default.gd"),
+	preload("./stat/stat_display_base.gd"),
+	preload("./stat/stat_display_default.gd"),
+]
+
+# Every once ands a while the script docs simply refuse to update properly.
+# This nudges the docs into a ensuring that the important scripts added by
+# this addon are actually loaded.
+func _ensure_script_docs():
+	var edit := get_editor_interface().get_script_editor()
+	for scr in ENSURE_SCRIPT_DOCS:
+		edit.update_docs_from_script(scr)
+
 func _get_plugin_name():
 	return PLUGIN_NAME
 
@@ -17,12 +35,14 @@ func _get_plugin_icon():
 	return PLUGIN_ICON
 
 func _enable_plugin() -> void:
+	_ensure_script_docs()
 	_try_add()
 
 func _disable_plugin() -> void:
 	_try_remove()
 
 func _enter_tree() -> void:
+	_ensure_script_docs()
 	if EditorInterface.is_plugin_enabled(PLUGIN_NAME):
 		_try_add()
 
--- addons/rpg_stats_effectable/plugin.gd
+++ addons/rpg_stats_effectable/plugin.gd
@@ -7,8 +7,27 @@ const PLUGIN_NAME := "rpg_stats"
 ## The icon of this plugin.
 const PLUGIN_ICON :Texture2D = preload("./icon.svg")
 
+const ENSURE_SCRIPT_DOCS:Array[Script] = [
+	preload("./effect/stat_effect.gd"),
+	preload("./set/stat_set_effectable.gd"),
+]
+
+# Every once ands a while the script docs simply refuse to update properly.
+# This nudges the docs into a ensuring that the important scripts added by
+# this addon are actually loaded.
+func _ensure_script_docs():
+	var edit := get_editor_interface().get_script_editor()
+	for scr in ENSURE_SCRIPT_DOCS:
+		edit.update_docs_from_script(scr)
+
 func _get_plugin_name():
 	return PLUGIN_NAME
 
 func _get_plugin_icon():
 	return PLUGIN_ICON
+
+func _enter_tree():
+	_ensure_script_docs()
+
+func _enable_plugin():
+	_ensure_script_docs()
